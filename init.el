2a16282f8f965d8eda642791e9c6170e2f533ec0
Christian Neeb <chneeb@gmail.com>
2009-06-21 22:46:02 -0400

    cleaned up Ruby config

--- init.el
+++ init.el
@@ -78,35 +78,13 @@
 ;; (global-set-key "\C-xm" 'browse-url-at-point)
 
 ;; Ruby stuff
-(autoload 'ruby-mode "ruby-mode" "Major mode for editing ruby scripts." t)
-(setq auto-mode-alist  (cons '(".rb$" . ruby-mode) auto-mode-alist))
-(setq auto-mode-alist  (cons '(".rhtml$" . html-mode) auto-mode-alist))
-
-(add-hook 'ruby-mode-hook
-          (lambda()
-            (add-hook 'local-write-file-hooks
-                      '(lambda()
-                         (save-excursion
-                           (untabify (point-min) (point-max))
-                           (delete-trailing-whitespace)
-                           )))
-            (set (make-local-variable 'indent-tabs-mode) 'nil)
-            (set (make-local-variable 'tab-width) 2)
-            (imenu-add-to-menubar "IMENU")
-	    ;;            (define-key ruby-mode-map "C-m" 'newline-and-indent) ;Not sure if this line is 100% right but it works!
-            (require 'ruby-electric)
-            (ruby-electric-mode t)
-            ))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
-(autoload 'inf-ruby-keys "inf-ruby"
-  "Set local key defs for inf-ruby in ruby-mode")
-(add-hook 'ruby-mode-hook
-	  '(lambda ()
-	     (inf-ruby-keys)))
-;; If you have Emacs 19.2x or older, use rubydb2x                              
-(autoload 'rubydb "rubydb3x" "Ruby debugger" t)
+(require 'ruby-mode)
+(require 'ruby-electric)
+(defun chneeb-ruby-mode-hook()
+  (font-lock-mode t)
+  (setq standard-indent 2)
+  (ruby-electric-mode t))
+(add-hook 'ruby-mode-hook 'chneeb-ruby-mode-hook)
 
 ;; Clojure
 (require 'clojure-mode)
