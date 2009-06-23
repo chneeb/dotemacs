02cf246fa99f9672477bf3e28e0213d995b5b6dd
Christian Neeb <chneeb@gmail.com>
2009-06-23 00:13:49 -0400

    adding configuration for wl

--- init.el
+++ init.el
@@ -2,6 +2,10 @@
 (setq home-path (expand-file-name "~"))
 (setq vendor-path (concat home-path "/.emacs.d/elisp"))
 
+;; Setting load path
+(add-to-list 'load-path vendor-path)
+(progn (cd vendor-path) (normal-top-level-add-subdirs-to-load-path))
+
 ;; Basic settings
 (blink-cursor-mode 0)
 (setq visible-bell t)
@@ -19,16 +23,6 @@
 (setq show-paren-delay 0)
 (show-paren-mode t)
 (setq show-paren-style 'parenthesis) ;'expression, 'parenthesis or 'mixed
-
-;; Setting load path
-(add-to-list 'load-path (concat vendor-path))
-(add-to-list 'load-path (concat vendor-path "/bbdb-2.35/lisp"))
-(add-to-list 'load-path (concat vendor-path "/emacs-w3m-1.4.4"))
-(add-to-list 'load-path (concat vendor-path "/color-theme-6.6.0"))
-(add-to-list 'load-path (concat vendor-path "/ruby-mode"))
-(add-to-list 'load-path (concat vendor-path "/org-6.19b/lisp"))
-(add-to-list 'load-path (concat vendor-path "/remember-1.9"))
-(add-to-list 'load-path (concat vendor-path "/clojure-mode"))
 
 ;; Color theme stuff
 (require 'color-theme)
@@ -75,7 +69,7 @@
 (setq w3m-use-cookies t)
 (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
 ;; optional keyboard short-cut
-;; (global-set-key "\C-xm" 'browse-url-at-point)
+(global-set-key "\C-xu" 'browse-url-at-point)
 
 ;; Ruby stuff
 (require 'ruby-mode)
@@ -91,6 +85,66 @@
 (setq clojure-src-root "/Users/chneeb/Source")
 (setq swank-clojure-extra-classpaths nil)
 (clojure-slime-config)
+
+(setq ssl-program-name "openssl")
+(setq ssl-program-arguments '("s_client" "-quiet" "-host" host "-port" service))
+
+;; wanderlust
+(autoload 'wl "wl" "Wanderlust" t)
+(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
+(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+
+;; IMAP
+(setq elmo-imap4-default-server "imap.gmail.com")
+(setq elmo-imap4-default-user "chneeb@gmail.com") 
+(setq elmo-imap4-default-authenticate-type 'clear) 
+(setq elmo-imap4-default-port '993)
+(setq elmo-imap4-default-stream-type 'ssl)
+
+(setq elmo-imap4-use-modified-utf7 t) 
+
+;; SMTP
+(setq wl-smtp-connection-type 'starttls)
+(setq wl-smtp-posting-port 587)
+(setq wl-smtp-authenticate-type "plain")
+(setq wl-smtp-posting-user "chneeb")
+(setq wl-smtp-posting-server "smtp.gmail.com")
+(setq wl-local-domain "gmail.com")
+
+(setq wl-from "Christian Neeb <chneeb@gmail.com>")
+(setq wl-draft-enable-queuing t)
+(if (eq window-system 'mac)
+    (setq wl-stay-folder-window t))
+(setq wl-default-folder "%inbox")
+(setq wl-default-spec "%")
+(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
+(setq wl-trash-folder "%[Gmail]/Trash")
+
+(setq wl-folder-check-async t) 
+
+(setq elmo-imap4-use-modified-utf7 t)
+
+(autoload 'wl-user-agent-compose "wl-draft" nil t)
+(if (boundp 'mail-user-agent)
+    (setq mail-user-agent 'wl-user-agent))
+(if (fboundp 'define-mail-user-agent)
+    (define-mail-user-agent
+      'wl-user-agent
+      'wl-user-agent-compose
+      'wl-draft-send
+      'wl-draft-kill
+      'mail-send-hook))
+
+;; Zoom
+(defun djcb-zoom (n)
+  "with positive N, increase the font size, otherwise decrease it"
+  (set-face-attribute 'default (selected-frame) :height 
+    (+ (face-attribute 'default :height) (* (if (> n 0) 1 -1) 10))))
+
+(global-set-key (kbd "C-+")      '(lambda nil (interactive) (djcb-zoom 1)))
+(global-set-key [C-kp-add]       '(lambda nil (interactive) (djcb-zoom 1)))
+(global-set-key (kbd "C--")      '(lambda nil (interactive) (djcb-zoom -1)))
+(global-set-key [C-kp-subtract]  '(lambda nil (interactive) (djcb-zoom -1)))
 
 ;; Own stuff
 (defun gcal ()
@@ -112,4 +166,8 @@
 ;; - JDEE
 ;; - eudcb-mab.el
 ;; - whitespace-mode -- http://www.emacswiki.org/emacs/WhiteSpace
+;; - http://www.djcbsoftware.nl/dot-emacs.html
+;; - http://www.emacswiki.org/cgi-bin/wiki/FileNameCache
+;; - http://www.busydoingnothing.co.uk/twitter-el/
+;; - http://www.emacswiki.org/emacs/hgw-init-wl.el
 ;; - http://www.djcbsoftware.nl/dot-emacs.html
