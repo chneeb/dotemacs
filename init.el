70e73637e8bff8cc8203989f07f4e6aacce737d0
Christian Neeb <chneeb@gmail.com>
2009-12-15 22:02:58 -0500

    re-enabling w3m

--- init.el
+++ init.el
@@ -70,12 +70,12 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 ;; W3M Stuff
-;; (require 'w3m)
-;; (setq browse-url-browser-function 'w3m-browse-url)
-;; (setq w3m-use-cookies t)
-;; (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-;;;; optional keyboard short-cut
-;; (global-set-key "\C-xu" 'browse-url-at-point)
+(require 'w3m)
+(setq browse-url-browser-function 'w3m-browse-url)
+(setq w3m-use-cookies t)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+;; optional keyboard short-cut
+(global-set-key "\C-xu" 'browse-url-at-point)
 
 ;; Ruby stuff
 (require 'ruby-mode)
