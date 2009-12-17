02cdca5797f0f71129e64ac51a93c1ff149a232b
Christian Neeb <chneeb@gmail.com>
2009-12-17 08:09:57 -0500

    adding opacity modify function

--- init.el
+++ init.el
@@ -75,6 +75,24 @@
        (twitter-time-stamp-face ((t (:bold nil :foreground "white" :background "blue")))))))
 (color-theme-djcb-dark)
 
+;; Opacity
+(defun djcb-opacity-modify (&optional dec)
+  "modify the transparency of the emacs frame; if DEC is t,
+    decrease the transparency, otherwise increase it in 10%-steps"
+  (let* ((alpha-or-nil (frame-parameter nil 'alpha)) ; nil before setting
+          (oldalpha (if alpha-or-nil alpha-or-nil 100))
+          (newalpha (if dec (- oldalpha 10) (+ oldalpha 10))))
+    (when (and (>= newalpha frame-alpha-lower-limit) (<= newalpha 100))
+      (modify-frame-parameters nil (list (cons 'alpha newalpha))))))
+
+ ;; C-8 will increase opacity (== decrease transparency)
+ ;; C-9 will decrease opacity (== increase transparency
+ ;; C-0 will returns the state to normal
+(global-set-key (kbd "C-8") '(lambda()(interactive)(djcb-opacity-modify)))
+(global-set-key (kbd "C-9") '(lambda()(interactive)(djcb-opacity-modify t)))
+(global-set-key (kbd "C-0") '(lambda()(interactive)
+                               (modify-frame-parameters nil `((alpha . 100)))))
+
 ;; BBDB stuff
 (require 'bbdb)
 (bbdb-initialize)
