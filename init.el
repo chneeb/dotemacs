89945e2d6b4b1f292cf278c5020b032a54e51c49
Christian Neeb <chneeb@chneeb.(none)>
2010-01-02 21:40:42 -0500

    Removing Wanderlust in favor of gnus

--- init.el
+++ init.el
@@ -155,58 +155,30 @@
 (setq ssl-program-name "openssl")
 (setq ssl-program-arguments '("s_client" "-quiet" "-host" host "-port" service))
 
-;; wanderlust
-(autoload 'wl "wl" "Wanderlust" t)
-(autoload 'wl-other-frame "wl" "Wanderlust on new frame." t)
-(autoload 'wl-draft "wl-draft" "Write draft with Wanderlust." t)
+;; gnus
+(require 'gnus)
 
-;; Fields in the e-mail header that I do not want to see (regexps)
-(setq wl-message-ignored-field-list (quote (".*Received:" ".*Path:" ".*Id:" "^References:" "^Replied:" "^Errors-To:" "^Lines:" "^Sender:" ".*Host:" "^Xref:" "^Content-Type:" "^Precedence:" "^Status:" "^X-VM-.*:" "^List-*" "^Authentication-Results*" "^X-*" "^Received-SPF*" "^DKIM-Signature:" "^DomainKey-Signature:" "^X-Mailman-Version:")))
-;; Fields in the e-mail header that I want to see even if they match the regex in wl-message-ignored-field-list
-(setq wl-message-visible-field-list (quote ("^Dnas.*:" "^Message-Id:" "^X-Mailer:" "^X-Mailman-Version:")))
+(setq user-mail-address "chneeb@gmail.com")
+(setq user-full-name "Christian Neeb")
 
-;; IMAP
-(setq elmo-imap4-default-server "imap.gmail.com")
-(setq elmo-imap4-default-user "chneeb@gmail.com") 
-(setq elmo-imap4-default-authenticate-type 'clear) 
-(setq elmo-imap4-default-port '993)
-(setq elmo-imap4-default-stream-type 'ssl)
+(setq gnus-select-method '(nnimap "imap.gmail.com"
+           (nnimap-address "imap.gmail.com")
+           (nnimap-server-port 993)
+           (nnimap-authinfo-file "~/.authinfo")
+           (nnimap-stream ssl)))
 
-(setq elmo-imap4-use-modified-utf7 t) 
+(setq smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      send-mail-function 'smtpmail-send-it
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-service 587
+      smtpmail-auth-credentials '(("smtp.gmail.com"
+       587
+       "chneeb@gmail.com"
+       nil)))
 
-;; SMTP
-(setq wl-smtp-connection-type 'starttls)
-(setq wl-smtp-posting-port 587)
-(setq wl-smtp-authenticate-type "plain")
-(setq wl-smtp-posting-user "chneeb")
-(setq wl-smtp-posting-server "smtp.gmail.com")
-(setq wl-local-domain "gmail.com")
-
-(setq wl-from "Christian Neeb <chneeb@gmail.com>")
-(setq wl-draft-enable-queuing t)
-(if (eq window-system 'mac)
-    (setq wl-stay-folder-window t))
-(setq wl-default-folder "%inbox")
-(setq wl-default-spec "%")
-(setq wl-draft-folder "%[Gmail]/Drafts") ; Gmail IMAP
-(setq wl-trash-folder "%[Gmail]/Trash")
-
-(setq wl-folder-check-async t) 
-
-(setq elmo-imap4-use-modified-utf7 t)
-
-(autoload 'wl-user-agent-compose "wl-draft" nil t)
-(if (boundp 'mail-user-agent)
-    (setq mail-user-agent 'wl-user-agent))
-(if (fboundp 'define-mail-user-agent)
-    (define-mail-user-agent
-      'wl-user-agent
-      'wl-user-agent-compose
-      'wl-draft-send
-      'wl-draft-kill
-      'mail-send-hook))
-
-(setq wl-folders-file (concat dotemacs-path "/wl-folders"))
+(add-hook 'gnus-topic-mode-hook 'gnus-topic-mode)
 
 ;; Twitter
 (require 'twitter)
