196d5822a02ad25ec47a9155c9403eb71f526ff7
Christian Neeb <chneeb@gmail.com>
2010-01-03 09:08:25 -0500

    adding work imap server

--- init.el
+++ init.el
@@ -161,11 +161,18 @@
 (setq user-mail-address "chneeb@gmail.com")
 (setq user-full-name "Christian Neeb")
 
-(setq gnus-select-method '(nnimap "imap.gmail.com"
-           (nnimap-address "imap.gmail.com")
-           (nnimap-server-port 993)
-           (nnimap-authinfo-file "~/.authinfo")
-           (nnimap-stream ssl)))
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")
+	       (nnimap-server-port 993)
+	       (nnimap-authinfo-file "~/.authinfo")
+	       (nnimap-stream ssl)))
+
+(setq gnus-secondary-select-methods
+      '((nnimap "work"
+		 (nnimap-address "145.228.237.176")
+		 (nnimap-server-port 143))
+	(nntp "news.gmane.org")))
 
 (setq smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
       smtpmail-smtp-server "smtp.gmail.com"
