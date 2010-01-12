7e1596400363e041aa39688f6b4f2f519936f43d
Christian Neeb <chneeb@gmail.com>
2010-01-12 12:28:33 -0500

    moving djcb to extra file

---
+++ djcb.el
@@ -0,0 +1,67 @@
+;; Color theme
+(defun color-theme-djcb-dark ()
+  "dark color theme created by djcb, Jan. 2009."
+  (interactive)
+  (color-theme-install
+    '(color-theme-djcb-dark
+       ((foreground-color . "#a9eadf")
+         (background-color . "black") 
+         (background-mode . dark))
+       (bold ((t (:bold t))))
+       (bold-italic ((t (:italic t :bold t))))
+       (default ((t (nil))))
+       
+       (font-lock-builtin-face ((t (:italic t :foreground "#a96da0"))))
+       (font-lock-comment-face ((t (:italic t :foreground "#bbbbbb"))))
+       (font-lock-comment-delimiter-face ((t (:foreground "#666666"))))
+       (font-lock-constant-face ((t (:bold t :foreground "#197b6e"))))
+       (font-lock-doc-string-face ((t (:foreground "#3041c4"))))
+       (font-lock-doc-face ((t (:foreground "gray"))))
+       (font-lock-reference-face ((t (:foreground "white"))))
+       (font-lock-function-name-face ((t (:foreground "#356da0"))))
+       (font-lock-keyword-face ((t (:bold t :foreground "#bcf0f1"))))
+       (font-lock-preprocessor-face ((t (:foreground "#e3ea94"))))
+       (font-lock-string-face ((t (:foreground "#ffffff"))))
+       (font-lock-type-face ((t (:bold t :foreground "#364498"))))
+       (font-lock-variable-name-face ((t (:foreground "#7685de"))))
+       (font-lock-warning-face ((t (:bold t :italic nil :underline nil 
+                                     :foreground "yellow"))))
+       (hl-line ((t (:background "#112233"))))
+       (mode-line ((t (:foreground "#ffffff" :background "#333333"))))
+       (region ((t (:foreground nil :background "#555555"))))
+       (show-paren-match-face ((t (:bold t :foreground "#ffffff" 
+                                    :background "#050505"))))
+       (twitter-user-name-face ((t (:bold t :foreground "white"  :background "blue"))))
+       (twitter-header-face ((t (:bold t :foreground "white"  :background "blue"))))
+       (twitter-time-stamp-face ((t (:bold nil :foreground "white" :background "blue")))))))
+
+;; Opacity
+(defun djcb-opacity-modify (&optional dec)
+  "modify the transparency of the emacs frame; if DEC is t,
+    decrease the transparency, otherwise increase it in 10%-steps"
+  (let* ((alpha-or-nil (frame-parameter nil 'alpha)) ; nil before setting
+          (oldalpha (if alpha-or-nil alpha-or-nil 100))
+          (newalpha (if dec (- oldalpha 10) (+ oldalpha 10))))
+    (when (and (>= newalpha frame-alpha-lower-limit) (<= newalpha 100))
+      (modify-frame-parameters nil (list (cons 'alpha newalpha))))))
+
+ ;; C-8 will increase opacity (== decrease transparency)
+ ;; C-9 will decrease opacity (== increase transparency
+ ;; C-0 will returns the state to normal
+(global-set-key (kbd "C-8") '(lambda()(interactive)(djcb-opacity-modify)))
+(global-set-key (kbd "C-9") '(lambda()(interactive)(djcb-opacity-modify t)))
+(global-set-key (kbd "C-0") '(lambda()(interactive)
+                               (modify-frame-parameters nil `((alpha . 100)))))
+
+;; Zoom
+(defun djcb-zoom (n)
+  "with positive N, increase the font size, otherwise decrease it"
+  (set-face-attribute 'default (selected-frame) :height 
+    (+ (face-attribute 'default :height) (* (if (> n 0) 1 -1) 10))))
+
+(global-set-key (kbd "C-+")      '(lambda nil (interactive) (djcb-zoom 1)))
+(global-set-key [C-kp-add]       '(lambda nil (interactive) (djcb-zoom 1)))
+(global-set-key (kbd "C--")      '(lambda nil (interactive) (djcb-zoom -1)))
+(global-set-key [C-kp-subtract]  '(lambda nil (interactive) (djcb-zoom -1)))
+
+(provide 'djcb)
