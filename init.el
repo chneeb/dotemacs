ee63264a2748807446e4dad8cf54dcaab141ef85
Christian Neeb <chneeb@gmail.com>
2012-04-09 10:19:41 -0300

    Adding Quack for Scheme

--- init.el
+++ init.el
@@ -194,3 +194,4 @@
 ;; - AceJump
 ;; - EmacsRocks Screencasts - http://emacsrocks.com
 ;; - Sauron - http://emacs-fu.blogspot.com/2011/12/sauron-keeping-eye-on-whats-going-on.html
+;; - Quack for Scheme - http://www.neilvandyke.org/quack/
